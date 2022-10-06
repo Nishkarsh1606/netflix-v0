81023fa83e4445b14b2bb3a4386e0853ec361f05
Nishkarsh-Mac <nishkarsh2912@gmail.com>
2022-10-06 12:26:39 +0530

    add cover & title with api call for featured movie

--- public/script.js
+++ public/script.js
@@ -1,6 +1,39 @@
-// // https://www.themoviedb.org/documentation/api
+/*
+1. Cover image for the featured movie
+2. Functions to derive all movies types
+3. Loop and display the array with url
+https://api.themoviedb.org/3/discover/tv?api_key=dd4d3b66bfd5b093fcb316890e461252&with_networks=213
+https://api.themoviedb.org/3/trending/movie/week?api_key=dd4d3b66bfd5b093fcb316890e461252
+https://api.themoviedb.org/3/movie/top_rated?api_key=dd4d3b66bfd5b093fcb316890e461252&language=en-US&page=1
+https://image.tmdb.org/t/p/w500/${imageURL} -->backdrop
+https://image.tmdb.org/t/p/w500/tVxDe01Zy3kZqaZRNiXFGDICdZk.jpg 
+*/
 
-// const getFeaturedMovie=async()=>{
-//     const response=await fetch('')
-//     const data= response.json()
+const apiKeyV3='dd4d3b66bfd5b093fcb316890e461252'
+const getOriginalURL=`https://api.themoviedb.org/3/discover/tv?api_key=${apiKeyV3}&with_networks=213`   //without trailer
+const getTrendingURL=`https://api.themoviedb.org/3/trending/movie/week?api_key=${apiKeyV3}`     
+const getTopRatedURL=`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKeyV3}&language=en-US&page=1`
+const imgBaseURL=`https://image.tmdb.org/t/p/w500`
+
+
+const featuredMovieTitle=document.getElementById('featuredMovieTitle')
+const featuredMovieBg=document.getElementById('featuredMovieBg')
+const netflixOriginals=document.getElementById('netflixOriginals')
+const trendingNow=document.getElementById('trendingNow')
+const topRated=document.getElementById('trendingNow')
+
+// window.onload=()=>{
+
 // }
+
+async function getFeaturedMovie(){
+    const randomPicker=Math.floor(Math.random()*20)
+    const result=await fetch('https://api.themoviedb.org/3/movie/popular?api_key=dd4d3b66bfd5b093fcb316890e461252&language=en-US&page=1')
+    const data=await result.json()
+    let selectedMovie=data.results[randomPicker]
+    featuredMovieBg.style.background=`url(${imgBaseURL}${selectedMovie.backdrop_path}) no-repeat fixed center center/cover`
+    featuredMovieTitle.textContent=selectedMovie.original_title
+    document.getElementById('featuredMovieDesc').textContent=selectedMovie.overview.substring(0,150)+`...`
+}
+
+getFeaturedMovie()
