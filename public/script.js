9971321a2810b0c35de0174aae98e53144cbce99
NishkarshS-iMac <ns513@student.london.ac.uk>
2022-10-21 02:33:11 +0530

    Revert "clean commit tree"

--- public/script.js
+++ public/script.js
@@ -52,19 +52,19 @@ async function getTopRated() {
 }
 
 //function to display movie information
-displayMovieDetails=(data,targetDOMElement)=>{
-    if(targetDOMElement===netflixOriginalShows){
-        for(let i=0;i<data.results.length;++i){
-            let moviePoster=`${imgBaseURL}${data.results[i].poster_path}`
-            netflixOriginalShows.innerHTML+=`
-            <div class="movie-card sm:w-[10vw] space-x-2 movie-identifier" style="background:url(${moviePoster})" ${onclick=displayTrailer}></div>
+displayMovieDetails = (data, targetDOMElement) => {
+    if (targetDOMElement === netflixOriginalShows) {
+        for (let i = 0; i < data.results.length; ++i) {
+            let moviePoster = `${imgBaseURL}${data.results[i].poster_path}`
+            netflixOriginalShows.innerHTML += `
+            <div class="movie-card sm:w-[10vw] space-x-2 movie-identifier" style="background:url(${moviePoster})"></div>
             `
         }
     }
-    else{
-        for(let i=0;i<data.results.length;++i){
-            let moviePoster=`${imgBaseURL}${data.results[i].backdrop_path}`
-            targetDOMElement.innerHTML+=`
+    else {
+        for (let i = 0; i < data.results.length; ++i) {
+            let moviePoster = `${imgBaseURL}${data.results[i].backdrop_path}`
+            targetDOMElement.innerHTML += `
             <div class='small-movie-card min-h-[100%] min-w-[15%] movie-identifier' style="background:url(${moviePoster});background-size: cover;background-repeat: no-repeat;background-position: center; object-fit:scale-down"></div>
             `
         }
@@ -113,34 +113,3 @@ window.onclick = function (event) {
         modal.style.display = "none";
     }
 }
-
-
-
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = displayTrailer
-
-function displayTrailer(){
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
